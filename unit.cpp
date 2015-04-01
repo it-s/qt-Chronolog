67e38abf12663beb1eb22bc40798696de84807d0
Eugene Trounev <eugene.trounev@gmail.com>
2015-03-31 21:53:58 -0400

    Additional functionality to unit scaler

--- unit.cpp
+++ unit.cpp
@@ -1,24 +1,38 @@
 #include "unit.h"
 #include <QtMath>
 #include <QDebug>
-Unit::Unit(const QSize& screenSize, const QSize& designedForSize, QObject *parent) :
+Unit::Unit(const QSize& screenSize, QObject *parent) :
     QObject(parent)
 {
-    mCurrentSize = screenSize;
-    mIntendedSize = designedForSize;
-    mRoudUp = false;
-
-    computeRatio();
+    init(screenSize);
 }
 
-Unit::~Unit()
+Unit::Unit(const QSize &screenSize, const QSize &designedForSize, QObject *parent) :
+    QObject(parent)
 {
+    init(screenSize, designedForSize);
+}
 
+Unit::Unit(const QSize &screenSize, const QSize &designedForSize, const qreal ratioMin, QObject *parent) :
+    QObject(parent)
+{
+    init(screenSize, designedForSize, ratioMin);
+}
+
+Unit::Unit(const QSize &screenSize, const QSize &designedForSize, const qreal ratioMin, const qreal ratioMax, QObject *parent) :
+    QObject(parent)
+{
+    init(screenSize, designedForSize, ratioMin, ratioMax);
+}
+
+Unit::Unit(const QSize &screenSize, const QSize &designedForSize, const qreal ratioMin, const qreal ratioMax, const bool roundUp, QObject *parent) :
+    QObject(parent)
+{
+    init(screenSize, designedForSize, ratioMin, ratioMax, roundUp);
 }
 
 qreal Unit::pt(int pixel)
 {
-
     return qreal(pixel) / mRatio;
 }
 
@@ -42,9 +56,8 @@ QString Unit::size()
     return QString("%1 x %2").arg(mCurrentSize.width()).arg(mCurrentSize.height());
 }
 
-void Unit::setRoundUp(bool r)
+void Unit::roundUp(bool r)
 {
-
     mRoudUp = r;
 }
 
@@ -57,10 +70,24 @@ void Unit::computeRatio()
     qreal appd =  qSqrt(qPow(mCurrentSize.width(),2)+ qPow(mCurrentSize.height(), 2));
 
     //Calculate the ration between what IS and what SHOULD be
-    mRatio =  appd/d;
+    mRatio = appd/d;
+
+    if (qAbs(mRatio) < qAbs(mRatioMin)) mRatio = mRatioMin;
+    if (qAbs(mRatio) > qAbs(mRatioMax)) mRatio = mRatioMax;
 
     qDebug()<<"RATIO COMPUTED "<<mRatio;
 
     emit ratioChanged();
+}
+
+void Unit::init(const QSize &screenSize, const QSize &designedForSize, const qreal ratioMin, const qreal ratioMax, const bool roundUp)
+{
+    mCurrentSize = screenSize;
+    mIntendedSize = designedForSize;
+    mRatioMin = ratioMin;
+    mRatioMax = ratioMax;
+    mRoudUp = roundUp;
+
+    computeRatio();
 }
 
--- unit.h
+++ unit.h
@@ -7,15 +7,18 @@ class Unit : public QObject
     Q_OBJECT
 
 public:
+    explicit Unit(const QSize& screenSize, QObject *parent = 0);
     explicit Unit(const QSize& screenSize, const QSize &designedForSize, QObject *parent = 0);
-    ~Unit();
+    explicit Unit(const QSize& screenSize, const QSize &designedForSize, const qreal ratioMin, QObject *parent = 0);
+    explicit Unit(const QSize& screenSize, const QSize &designedForSize, const qreal ratioMin, const qreal ratioMax, QObject *parent = 0);
+    explicit Unit(const QSize& screenSize, const QSize &designedForSize, const qreal ratioMin, const qreal ratioMax, const bool roundUp, QObject *parent = 0);
 
     Q_INVOKABLE qreal pt(int pixel);
     Q_INVOKABLE int px(qreal point);
     Q_INVOKABLE int scale(qreal point, qreal ratio);
     Q_INVOKABLE qreal ratio();
     Q_INVOKABLE QString size();
-    Q_INVOKABLE void setRoundUp(bool v);
+    Q_INVOKABLE void roundUp(bool v);
 
 protected slots:
     void computeRatio();
@@ -23,12 +26,15 @@ protected slots:
 signals:
     void ratioChanged();
 
-
    private:
     bool mRoudUp;
     QSize mCurrentSize;
     QSize mIntendedSize;
     qreal mRatio;
+    qreal mRatioMin;
+    qreal mRatioMax;
+
+    void init(const QSize& screenSize, const QSize &designedForSize = QSize(640,480), const qreal ratioMin = 0.0f, const qreal ratioMax = 99.0f, const bool roundUp = false);
 
 };
 
